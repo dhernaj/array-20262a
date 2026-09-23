3f5ee2190a6bac8f0596d2870007e42cbd4bc02b
Danny Herrera <dhernaj@gmail.com>
2026-09-23 09:13:19 -0500

    Impresión de notas funcion

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
-﻿int[] notas={15,18,12,17,20};
+﻿//FUnción de Imprimir notas
+static void ImprimirNotas(int[] notas)
+{
+    for(int i=0; i<notas.Length; i++)
+    {
+        Console.WriteLine(notas[i]);
+    }
+}
+
+
+int[] notas={15,18,12,17,20};
 
 //Imprimir las notas
-for(int i=0; i<=notas.Length; i++)
-{
-    Console.WriteLine(notas[i]);
-}
+ImprimirNotas(notas);
+
--- obj/Debug/net10.0/Array.AssemblyInfo.cs
+++ obj/Debug/net10.0/Array.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Array")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a4eff6bbdc3e2272bcec554b261d23a22aac9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Array")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Array")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

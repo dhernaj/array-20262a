9d3898971d04c121f58e02a34dd662f7d265ed10
Danny Herrera <dhernaj@gmail.com>
2026-09-23 10:08:04 -0500

    Calcular promedio

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿//FUnción de Imprimir notas
+﻿//función de Imprimir notas
 static void ImprimirNotas(int[] notas)
 {
     for(int i=0; i<notas.Length; i++)
@@ -7,9 +7,24 @@ static void ImprimirNotas(int[] notas)
     }
 }
 
+//Función Calcular promedio 
+static double CalcularPromedio(int[] notas)
+{
+    int suma=0;
+    for(int i=0; i<notas.Length; i++)
+    {
+        suma+=notas[i];
+    }
+    return (double)suma / notas.Length;
+}   
+
 
 int[] notas={15,18,12,17,20};
 
 //Imprimir las notas
 ImprimirNotas(notas);
+
+//Calcular el promedio de las notas
+double promedio = CalcularPromedio(notas);
+Console.WriteLine($"El promedio es: {promedio}");
 
--- obj/Debug/net10.0/Array.AssemblyInfo.cs
+++ obj/Debug/net10.0/Array.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Array")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a4eff6bbdc3e2272bcec554b261d23a22aac9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f5ee2190a6bac8f0596d2870007e42cbd4bc02b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Array")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Array")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

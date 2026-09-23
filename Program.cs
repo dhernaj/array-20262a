edff293988506bdfb8cfe39d9932d3447bafa5f1
Danny Herrera <dhernaj@gmail.com>
2026-09-23 10:12:16 -0500

    Imprimir Mayor y menor

--- Program.cs
+++ Program.cs
@@ -28,3 +28,19 @@ ImprimirNotas(notas);
 double promedio = CalcularPromedio(notas);
 Console.WriteLine($"El promedio es: {promedio}");
 
+// Imprimir las nota mayor y menor
+int mayor = notas[0];
+int menor = notas[0];
+for(int i=1; i<notas.Length; i++)
+{
+    if(notas[i] > mayor)
+    {
+        mayor = notas[i];
+    }
+    if(notas[i] < menor)
+    {
+        menor = notas[i];
+    }
+}
+Console.WriteLine($"La nota mayor es: {mayor}");
+Console.WriteLine($"La nota menor es: {menor}");
